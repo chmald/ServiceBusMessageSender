6e7f3d80ea26f15de6e37b5c44df9d144ff4d60c
Christopher Maldonado <chmald@microsoft.com>
2024-03-13 10:42:57 -0500

    Updates to logic to handle more than 2k messages

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -20,12 +20,36 @@ namespace ServiceBusMessageSender.Controllers
         public async Task<ActionResult> Index(
             [FromQuery(Name = "numMsgs")] int numMsgs = 0)
         {
+            int batchLimit = 2000;
+            int fullRuns = 0;
+            int remaining;
 
+            if(numMsgs <= batchLimit)
+            {
+                remaining = numMsgs;
+            } else
+            {
+                fullRuns = Convert.ToInt32(Math.Floor(Convert.ToDecimal(numMsgs / batchLimit)));
+                remaining = numMsgs - (fullRuns * batchLimit);
+            }
+
+            for(int i = 0; i < fullRuns; i++)
+            {
+                await RunBatch(batchLimit);
+            }
+
+            await RunBatch(remaining);
+
+            return Ok();
+        }
+
+        private async Task RunBatch(int messages)
+        {
             using ServiceBusMessageBatch messageBatch = await _serviceBusSender.CreateMessageBatchAsync();
 
-            for (int i = 0; i < numMsgs; i++)
+            for (int i = 0; i < messages; i++)
             {
-                if(!messageBatch.TryAddMessage( new ServiceBusMessage($"Message {i}")))
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
                 {
                     throw new Exception($"The message {i} is too large to fit in the batch.");
                 }
@@ -34,14 +58,12 @@ namespace ServiceBusMessageSender.Controllers
             try
             {
                 await _serviceBusSender.SendMessagesAsync(messageBatch);
-                Console.WriteLine($"A batch of {numMsgs} messages has been published to the queue.");
+                Console.WriteLine($"A batch of {messages} messages has been published to the queue.");
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
-
-            return Ok();
         }
     }
 }
